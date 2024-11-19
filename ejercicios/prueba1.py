61f579386ba534544d4c9d5b9acad7ca3782fa6e
spalpal955 <spalpal955@g.educaand.es>
2024-11-19 18:08:08 +0100

    cambio en prueba1.py

--- ejercicios/prueba1.py
+++ ejercicios/prueba1.py
@@ -1,5 +1,5 @@
 edad = input("Introduzca su edad: ")  
 if edad >= 18:  
-    print("Toma una cerveza!")  
+    print("Toma un cubata!")  
 else:  
     print(f"Toma un zumo de piña, con {edad} años eres menor.")
