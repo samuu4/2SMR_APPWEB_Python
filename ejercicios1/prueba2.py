c7fd571a8109eab3305d788242264566d7a7d07d
spalpal955 <spalpal955@g.educaand.es>
2024-11-19 17:52:50 +0100

    Creado el programa prueba2

---
+++ ejercicios1/prueba2.py
@@ -0,0 +1,4 @@
+num1 = int(input("Dame un número: "))  
+num2 = int(input("Dame otro: "))  
+print("La suma " + str(num1) + " + " + str(num2) + " es igual a " + str(num1 + num2))  
+print(f"{num1} + {num2} = {num1+num2}")  
